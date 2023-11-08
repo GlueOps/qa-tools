08bdcc6ce3dcb7840a8c055d47a26055155588e0
AntonBilyk <98952218+AntonBilyk@users.noreply.github.com>
2023-11-08 13:57:01 +0200

    Update 2.sh

--- 2/2.sh
+++ 2/2.sh
@@ -48,44 +48,44 @@ read -p "Enter arn:aws:iam number" value2
 
 # terraform apply -auto-approve
 
-cd /workspaces/glueops
+# cd /workspaces/glueops
 
-aws eks update-kubeconfig --region us-west-2 --name captain-cluster --role-arn arn:aws:iam::$value2:role/glueops-captain-role
+# aws eks update-kubeconfig --region us-west-2 --name captain-cluster --role-arn arn:aws:iam::$value2:role/glueops-captain-role
 
-sed -i 's/^#//' $CLUSTER.pluto.onglueops.rocks/.env
+# sed -i 's/^#//' $CLUSTER.pluto.onglueops.rocks/.env
 
-source /workspaces/glueops/$CLUSTER.pluto.onglueops.rocks/.env
+# source /workspaces/glueops/$CLUSTER.pluto.onglueops.rocks/.env
 
-kubectl get nodes
+# kubectl get nodes
 
-kubectl delete daemonset -n kube-system aws-node
+# kubectl delete daemonset -n kube-system aws-node
 
-cd /workspaces/glueops/$CLUSTER.pluto.onglueops.rocks
+# cd /workspaces/glueops/$CLUSTER.pluto.onglueops.rocks
 
-curl https://raw.githubusercontent.com/wiki/GlueOps/terraform-module-cloud-aws-kubernetes-cluster/calico.yaml.md -o ../qa-tools/2/templates2/calico.yaml 
+# curl https://raw.githubusercontent.com/wiki/GlueOps/terraform-module-cloud-aws-kubernetes-cluster/calico.yaml.md -o ../qa-tools/2/templates2/calico.yaml 
 
-# render_templates2() {
-#   local template_dir="$1"
-#   local target_dir="$PWD"
+render_templates2() {
+  local template_dir="$1"
+  local target_dir="$PWD"
 
-#   # Create the target directory if it doesn't exist
-#   mkdir -p "$target_dir"
+  # Create the target directory if it doesn't exist
+  mkdir -p "$target_dir"
 
-#   # Loop through the template files in the template directory
-#   for template_file in "$template_dir"/*; do
-#     if [ -f "$template_file" ]; then
-#       # Extract the filename without the path
-#       template_filename=$(basename "$template_file")
+  # Loop through the template files in the template directory
+  for template_file in "$template_dir"/*; do
+    if [ -f "$template_file" ]; then
+      # Extract the filename without the path
+      template_filename=$(basename "$template_file")
 
-#       # Delete yaml markers
-#       sed '/```yaml/,/```/d' "$template_file" > "$target_dir/$template_filename"
-#     fi
-#   done
+      # Delete yaml markers
+      sed '/```yaml/,/```/d' "$template_file" > "$target_dir/$template_filename"
+    fi
+  done
 
-#   echo "calico.yaml is successfully created in $target_dir."
-# }
+  echo "calico.yaml is successfully created in $target_dir."
+}
 
-# render_templates2 "../../../qa-tools/2/templates2" 
+render_templates2 "../../../qa-tools/2/templates2" 
 
 # raw_link="https://raw.githubusercontent.com/wiki/GlueOps/terraform-module-cloud-aws-kubernetes-cluster/install-calico.md"
 
