e57cadfe0378481b58d4ff7624bd4eaa4481a0d9
AntonBilyk <98952218+AntonBilyk@users.noreply.github.com>
2023-11-09 17:54:04 +0200

    Update 3.sh

--- 3/3.sh
+++ 3/3.sh
@@ -2,4 +2,12 @@
 
 source /workspaces/glueops/$CLUSTER.pluto.onglueops.rocks/.env
 
-kubectl -n glueops-core-vault port-forward svc/vault-ui 8200:8200
+kubectl -n glueops-core-vault port-forward svc/vault-ui 8200:8200 &
+
+sleep 5  
+
+cd /workspaces/glueops/$CLUSTER.pluto.onglueops.rocks/terraform/vault/configuration/
+
+terraform init && terraform apply -auto-approve
+
+exit
