83e5c77f90605e92ea4d35ca45ffb8ffb400da86
AntonBilyk <98952218+AntonBilyk@users.noreply.github.com>
2023-11-10 16:21:36 +0000

    update 4 and 5

--- 4/4.sh
+++ 4/4.sh
@@ -12,8 +12,6 @@ read -p "Enter helm-chart version: " value2
 
 kubectl apply -k "https://github.com/argoproj/argo-cd/manifests/crds?ref=$value1"
 
-helm diff upgrade argocd argo/argo-cd --version $value2 -f argocd.yaml -n glueops-core
-
 sleep 15
 
 helm upgrade argocd argo/argo-cd --version $value2 -f argocd.yaml -n glueops-core
--- 5/5.sh
+++ 5/5.sh
@@ -2,8 +2,6 @@
 
 read -p "Enter glueops-platform version: " value1
 
-helm diff upgrade glueops-platform glueops-platform/glueops-platform --version $value1 -f platform.yaml --namespace=glueops-core
-
 sleep 15
 
 helm upgrade glueops-platform glueops-platform/glueops-platform --version $value1 -f platform.yaml --namespace=glueops-core
