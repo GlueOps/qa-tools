e2438dd0c6ae185e71470848f454591c5d29c8ff
AntonBilyk <98952218+AntonBilyk@users.noreply.github.com>
2023-11-10 17:01:32 +0200

    Update 5.sh

--- 5/5.sh
+++ 5/5.sh
@@ -4,6 +4,6 @@ read -p "Enter glueops-platform version: " value1
 
 helm diff upgrade glueops-platform glueops-platform/glueops-platform --version value1 -f platform.yaml --namespace=glueops-core
 
-sleep 10
+sleep 15
 
 helm upgrade glueops-platform glueops-platform/glueops-platform --version value1 -f platform.yaml --namespace=glueops-core
