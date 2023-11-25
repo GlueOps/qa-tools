e6a2cea8d592801707d5cbce1964b150c8e3e540
AntonBilyk <98952218+AntonBilyk@users.noreply.github.com>
2023-11-25 02:54:40 +0000

    temporary commit

--- manifests-ecr-protected-script/generate-manifests-ecr-protected.sh
+++ manifests-ecr-protected-script/generate-manifests-ecr-protected.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
+read -p "Enter your cluster name: " CLUSTER
+
 cd /workspaces/glueops/$CLUSTER/manifests
 
 # Function to render templates
 render_templates() {
   local template_dir="$1"
   local target_dir="$PWD"
+  
+  # Split cluster name to different parts
 
-  # Prompt the user for the value of the variables
-  read -p "Enter placeholder value (test-80-np, test-82-np or test-1-np) " value
+  IFS='.' read -r ENV TENANT DOMAIN <<< "$CLUSTER"
 
-  read -p "Enter access-key-id for ecr-regcred: " value2
+  # Dcrregcred manifest creating
+  read -p "Do you want to create ecr-regcred.yaml manifest? Type 'yes' or 'no': " process_file
 
-  read -p "Enter secret-access-key: " value3
+  # Define ecrregcred manifest fullname
+  REGCRED="ecr-regcred.yaml"
+
+  if [ "$process_file" == no ]; then
+    echo "File processing skipped for $REGCRED."
+  else
+    read -p "Enter access-key-id for ecr-regcred: " value2
+    read -p "Enter secret-access-key: " value3
+  fi
+
+  # Organization and repository path
+  read -p "Do you use default organization(example-tenant) and repository(deployment-configurations)? Type 'yes' or 'no': " use_default
+  
+  if [ "$use_default" == yes ]; then
+    org_name="example-tenant"
+    repo_name="deployment-configurations"
+  else
+    read -p "Enter organization name: " org_name
+    read -p "Enter repository name: " repo_name
+  fi
 
   # Create the target directory if it doesn't exist
   mkdir -p "$target_dir"
@@ -24,7 +47,9 @@ render_templates() {
       template_filename=$(basename "$template_file")
 
       # Replace the placeholder with the user-entered value and save it to the target directory
-      sed "s/cluster_env/$value/g; s/key_id/$value2/g; s/key_value/$value3/g" "$template_file" > "$target_dir/$template_filename"
+      if [ "$process_file" == yes ] || [ "$template_filename" != "$REGCRED" ]; then
+        sed "s/cluster_env/$ENV/g; s/key_id/$value2/g; s/key_value/$value3/g; s/example-tenant/$org_name/g; s/deployment-configurations/$repo_name/g" "$template_file" > "$target_dir/$template_filename"
+      fi
     fi
   done
 
